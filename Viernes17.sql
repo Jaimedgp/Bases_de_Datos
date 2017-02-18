69d55999741d8fcec7147770a6a38d2a1dc76f6f
Jaime Díez González-Pardo <jaime.diez.gp@gmail.com>
2017-02-18 15:34:55 +0100

    Pasar a limpio los apuntes

--- Viernes17.sql
+++ Viernes17.sql
@@ -1,22 +1,26 @@
-/*
- * Apumtes de clase de bases
-*/
+/************************************************
+ ** APUNTES DE LA CLASE DE BASE DE DATOS       **
+ **  INGENIERIA INFORMATICA.                   **
+ **													Febrero 17, 2017   **
+ ***********************************************/
 
-CREATE TABLE PROPIETARIOS
-	(DNI CHAR(10) NOT NULL CONSTRAINt pk_prop PRIMARY KEY,
-	 NOMBRE CHAR(25) NOT NULL, 
-	 DIRECCION CHAR(30),
-	 PRIMARY KEY(DNI)); --se puede poner la primary key tanto en la fila como al final de la tabla
+create table PROPIETARIOS 
+	(dni 				char(10)	not null constraint pk_prop primary key,	--se puede poner la primary key en la misma fila
+	 nombre			char(25)	not null,
+	 direccion 	char(30)
+		/*PRIMARY KEY(DNI)*/);----se puede poner la primary key al final de la tabla 
 
-create table locales
-	(codigo char(10) not null constraint pk_prop primary key,
-	 dni char(10) not null,
-	 ubicacion char(4) not null,
-	 superficie DEC(8,2) not null,
-	 constraint fk_loc foreign key (dni) references propietarios(dni));
+create table LOCALES
+	(codigo			char(10)	not null,
+	 codigoPer	char(10)	not null,
+	 ubicacion	char(4)		not null,
+	 superficie	dec(8,2)	not null,	-- numero decimal de 8 cifras y 2 decimales --
+	 constraint fk_loc foreign key (codigoPer) references PROPIETARIOS(dni));	 
 
-
-
+create table JUGADORES_BALONCESTO
+	(dni				char(10)		not null,
+	 sexo				char(1)			not null	check(sexo in ('M', 'F'),
+	 fechaInsc	date				not null	default getdate());
 
 -- para insertar nuevas fila --
 insert into PROPIETARIOS(dni, NOMBRE, DIRECCION)
@@ -37,3 +41,4 @@ insert into PROPIETARIOS
  select codigo, ubicacion, NOMBRE, DIRECCION --columnas que devuelva
  	from locales, propietarios -- intervienen
  where Locales.DNI = PROPIETARIOS.DNI and SUPERFICIE > 200
+
