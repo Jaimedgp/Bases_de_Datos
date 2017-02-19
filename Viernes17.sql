4484063d98f268365d77eb825eac9f4d0b6f64fd
Jaime Díez González-Pardo <jaime.diez.gp@gmail.com>
2017-02-19 22:07:57 +0100

    mejorando los apuntes y los comentarios

--- Viernes17.sql
+++ Viernes17.sql
@@ -1,13 +1,23 @@
 /**************************************************
  *** APUNTES DE LA CLASE DE BASE DE DATOS       ***
  ***  INGENIERIA INFORMATICA.                   ***
- ***													Febrero 17, 2017  ***
+ ***						                                ***
+ ***           +----------------------+         ***
+ ***           |        CLASE1        |         ***
+ ***           +----------------------+         ***
+ ***           |     PROPIETARIOS     |         ***
+ ***           |       LOCALES        |         ***
+ ***           | JUGADORES_BALONCESTO |         ***
+ ***           +----------------------+         ***
+ ***                                            ***
+ ***                          Febrero 17, 2017  ***
  **************************************************/
 
-	/**
-	* CREAR TABLAS EN SQL
-	*/
+	-- Crear la base de datos --
+create database CLASE1(
 
+
+	-- Crear Tablas en la base de datos --
 create table PROPIETARIOS 
 	(dni 				char(10)	not null constraint pk_prop primary key,	--se puede poner la primary key en la misma fila
 	 nombre			char(25)	not null,
@@ -24,22 +34,33 @@ create table LOCALES
 create table JUGADORES_BALONCESTO
 	(dni				char(10)		not null,
 	 sexo				char(1)			not null	check(sexo in ('M', 'F')),
+	 edad				integer			not null,
+	 suma				float				not null,
 	 fechaInsc	date				not null	default getdate());
+);
 
 	/** 
 	* INSERTAR NUEVAS FILAS
+	*
+	*       +-----------+------------+-------------+
+	*       | dni       | nombre     | direccion   |
+	*       +-----------+------------+-------------+
+	*       | 13234567R | sanz Lluis | Gran via 26 |
+	*       +-----------+------------+-------------+
+	*       | 13234567R |    null    | Gran via 26 |
+	*       +-----------+------------+-------------+
 	*/
 
-insert into PROPIETARIOS(dni, nombre, diraccion)
+insert into PROPIETARIOS(dni, nombre, direccion)
 	values('13234567R', 'sanz Lluis', 'Gran via 26') 
+
+	-- cuando algun valor es nulo --
+insert into PROPIETARIOS
+	values ('13234567R', null, 'Gran via 26')
 
 -- tambien se puede escribir --
 insert into LOCALES
 	values ('13234567R', 'sanz Lluis', 'Gran via 26') 
-
--- cuando algun valor es nulo --
-insert into PROPIETARIOS
-	values('13234567R', null, 'Gran via 26') 
 
 	/*
 	* ENCONTRAR LOS LOCALES CON SUPERFICIE MAYOR QUE 200 Y SU PROPIETARIO
