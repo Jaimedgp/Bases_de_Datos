beb3e06a100a4bf8f1dde9c47a8b2d57455761b7
Jaime Díez González-Pardo <jaime.diez.gp@gmail.com>
2017-02-22 21:09:21 +0100

    new database

---
+++ DataBase1.sql
@@ -0,0 +1,29 @@
+/**
+ * PRIMERA PRUEBA CON BASES DE DATOS 
+ *            EN MYSQL
+ *
+ */
+
+create database if not exists JGP_PROJECTESES;
+
+use JGP_PROJECTESES;
+
+	create table if not exists DATA(
+		cod_project char(10),
+		name char(10),
+		numbers float,
+		primary key(cod_project));
+
+	create table if not exists PLOT(
+		types char(10), -- check('Linear','Exponential','Logarithmic','Polynomial','Sinusoidal'),
+		Xaxes char(10)
+		Yaxes char(10)
+		primary key(types),
+		constraint foreign key (Xaxes) references DATA(name));
+
+
+	create table if not exists PROJECTES(
+		path_directory varchar(20),
+		graphic char(10), -- check('Linear','Exponential','Logarithmic','Polynomial','Sinusoidal'),
+		data char(10),
+		primary key(data));
