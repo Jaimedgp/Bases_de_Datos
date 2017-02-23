16412e63765fa3eea647aa1b55206c6f44ecc7cb
Jaime Díez González-Pardo <jaime.diez.gp@gmail.com>
2017-02-23 17:32:21 +0100

    Add files via upload

---
+++ Practicas/Practica2/JDGP1_Practica2.sql
@@ -0,0 +1,24 @@
+insert into nacionalidad values ('Espanola')
+insert into nacionalidad values ('Inglesa')
+
+insert into duenio values ('12345678W', 'jaime', 'bondarieta', 'arrteta', 1)
+insert into duenio values ('12345678W', 'jaime', 'bondarieta', 'arrteta', 1)
+insert into duenio values ('12345678W', 'jaime', 'bondarieta', 'arrteta', 1)
+insert into duenio values ('12345678W', 'jaime', 'bondarieta', 'arrteta', 1)
+insert into duenio values ('12345678W', 'jaime', 'bondarieta', 'arrteta', 1)
+
+insert into especie values (1,'aleman')
+insert into especie values (2,'escoces')
+insert into especie values (3,'fox terrier')
+
+insert into mascota values ('lana', '1', 50.1, '1/01/2000', 1,1)
+insert into mascota values ('tata', '2', 51.2, '2/01/2000', 2,2)
+insert into mascota values ('rudolf', '3', 52.3, '3/01/2000', 3,3)
+
+insert into concurso values ('Monaco', 1000, '31/12/2016')
+insert into concurso values ('San Marino', 2000, '1/01/2017')
+insert into concurso values ('Andorra', 0.03, '3/06/2017')
+
+insert into registro values ('12345', 1, 1, '30/12/2016', 9.2, 'Parece suave')
+insert into registro values ('54321', 2, 2, '14/12/2016', null, null)
+insert into registro values ('67890', 2, 3, '16/2/2016', null, null)
