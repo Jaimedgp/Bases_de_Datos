b2aaff1e7e5a67ed3e4675e2d378451b98437e7b
Jaime Díez González-Pardo <jaime.diez.gp@gmail.com>
2017-05-04 22:22:49 +0200

    una prueba

---
+++ Apuntes/Jueves16.sql
@@ -0,0 +1,17 @@
+-- una lista es una tabla pblica -- la consulta realizada
+
+-- si se definen Dec(5,1) y Dec(2,7) no son del mismo tipo --
+
+-- varbinary(max) permite almacenar ficheros que no esten en la base de datos sino en el SO --
+
+set identity_insert db_name ON -- permite insertar manualmente el identity
+
+set identity_insert db_name off -- no permite insertar manualmente el identity
+
+-- with nocheck hace que no se ejecute el check hasta ese punto -- 
+
+
+
+
+
+
